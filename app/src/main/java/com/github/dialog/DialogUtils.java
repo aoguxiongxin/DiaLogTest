752256a7a08c46f0e8ba7cf4c5865c7acffca55d
zhaoqian1928 <360678360@qq.com>
2017-10-17 14:08:12 +0800

    版本更新3.0

--- app/src/main/java/com/github/dialog/DialogUtils.java
+++ app/src/main/java/com/github/dialog/DialogUtils.java
@@ -50,4 +50,24 @@ public class DialogUtils {
                 })
                 .show();
     }
+
+    public static void tipDiaLog(final Context context, final String title, String message) {
+        new AlertDialog.Builder(context)
+                .setIcon(R.mipmap.tip)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("领取", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context, "恭喜" + title + "明天到你家", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("不要", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context, "不要拉倒", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
+    }
 }
